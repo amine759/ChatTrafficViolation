e0e423b6038aef7fbfd6b37c82de5e065f22a33c
amine759 <saihiamine569@gmail.com>
2024-02-11 11:47:23 +0100

    good to go

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .chatbot import upsert_input, predict
 from django.http import JsonResponse
+import ast
 
 
 @csrf_exempt
@@ -22,7 +23,24 @@ def chatbot(request):
         if valid:
             upsert_input.delay(message, pred)  #
 
-        res = pred.replace("\n", "<br>")
+        prediction = ast.literal_eval(pred)
+        
+        chat_response = (
+            "Cette amende appartient à la classe : "
+            + str(prediction[0])
+            + "<br>"
+            + "Les points à retirer : "
+            + str(prediction[-1])
+            + "<br>"
+            + "Montant à payer en cas de règlement immédiat ou dans les 24 heures suivant l`infraction : "
+            + str(prediction[1])
+            + "<br>"
+            + "Si le règlement est effectué dans les 15 jours suivants : "
+            + str(prediction[2])
+        )
+
+        res = chat_response
+
         return JsonResponse({"res": res})
 
 
